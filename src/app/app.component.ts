950789df27522402b6365726330d0daa45d76876
zvercodebender <rbroker@digital.ai>
2021-12-16 14:19:22 -0500

    initial menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature = 'recipe';
   title = 'recipeApp';
+
+  onNavigate( feature: string ) {
+    console.log("onNavigate");
+    this.loadedFeature = feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,19 @@
 import { componentFactoryName } from '@angular/compiler';
+import { EventEmitter } from '@angular/core';
 import { Component } from '@angular/core';
+import { Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderCompont {
+    @Output() featureSelected = new EventEmitter<string>();
+
     collapsed = true;
+
+    onSelect( feature: string ) {
+        console.error("Setting feature to " + feature);
+        this.featureSelected.emit( feature );
+      }
 }
